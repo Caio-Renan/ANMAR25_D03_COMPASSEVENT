123ab9e211b7ba947962f795ad5e08c74a6dc3d6
Caio Renan <caiorenan016@gmail.com>
2025-05-28 11:42:11 -0300

    feat(main): add Swagger documentation setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   try {
@@ -13,6 +14,15 @@ async function bootstrap() {
         transform: true,
       }),
     );
+
+    const config = new DocumentBuilder()
+      .setTitle('Compass Event API')
+      .setDescription('API for managing users, events, and registrations')
+      .setVersion('0.0.1')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
 
     await app.listen(process.env.PORT ?? 3000);
   } catch (error) {
