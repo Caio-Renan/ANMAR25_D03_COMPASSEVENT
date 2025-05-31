7dd36462bbcdae9d5a8902b99eb56aa4669a9e18
Caio Renan <caiorenan016@gmail.com>
2025-05-31 16:10:47 -0300

    refactor(aws): register aws services in AwsModule

--- src/config/aws/aws.module.ts
+++ src/config/aws/aws.module.ts
@@ -1,13 +1,20 @@
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DynamoService } from 'src/common/aws/dynamo.service';
+import { S3Service } from 'src/common/aws/s3.service';
+import { SESService } from 'src/common/aws/ses.service';
+
 import { DynamoProvider } from './dynamo.provider';
 import { S3Provider } from './s3.provider';
 import { SESProvider } from './ses.provider';
 
+const awsProviders = [DynamoProvider, S3Provider, SESProvider];
+const awsServices = [DynamoService, S3Service, SESService];
+
 @Global()
 @Module({
   imports: [ConfigModule],
-  providers: [DynamoProvider, S3Provider, SESProvider],
-  exports: [DynamoProvider, S3Provider, SESProvider],
+  providers: [...awsProviders, ...awsServices],
+  exports: [...awsProviders, ...awsServices],
 })
 export class AwsModule {}
