6ad77d839c288a98f7c9462c44c82b3170a9535b
Caio Renan <caiorenan016@gmail.com>
2025-05-31 19:35:50 -0300

    chore(exceptions): add index.ts barrel file for exceptions folder

---
+++ src/common/exceptions/index.ts
@@ -0,0 +1,5 @@
+export * from './custom/email-already-exists.exception';
+export * from './custom/event-not-found.exception';
+export * from './custom/invalid-credentials.exception';
+export * from './custom/user-not-found.exception';
+export * from './filters/all-exceptions.filter';
