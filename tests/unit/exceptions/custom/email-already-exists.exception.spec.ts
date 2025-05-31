942aeff855f1f2bdcc71ea8e63ca6df410d03d31
Caio Renan <caiorenan016@gmail.com>
2025-05-31 19:27:02 -0300

    feat(exceptions, tests): add EmailAlreadyExistsException with unit tests

---
+++ tests/unit/exceptions/custom/email-already-exists.exception.spec.ts
@@ -0,0 +1,17 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+import { ExceptionErrorMessages } from 'src/common/constants/error-messages/exception-error-messages';
+
+import { EmailAlreadyExistsException } from '../../../../src/common/exceptions';
+
+describe('EmailAlreadyExistsException', () => {
+  it('should create an HttpException with status 409 and proper message', () => {
+    const exception = new EmailAlreadyExistsException();
+
+    expect(exception).toBeInstanceOf(HttpException);
+    expect(exception.getStatus()).toBe(HttpStatus.CONFLICT);
+    expect(exception.message).toBe(ExceptionErrorMessages.CUSTOM_EXCEPTION.EMAIL_ALREADY_EXISTS);
+    expect(exception.getResponse()).toBe(
+      ExceptionErrorMessages.CUSTOM_EXCEPTION.EMAIL_ALREADY_EXISTS,
+    );
+  });
+});
