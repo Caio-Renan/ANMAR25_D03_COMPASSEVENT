0bba4a74c73817854b9794a9f0b7273a36a94484
Caio Renan <caiorenan016@gmail.com>
2025-05-31 19:36:39 -0300

    chore(app): register global AllExceptionsFilter provider in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
+
+import { AllExceptionsFilter } from './common/exceptions';
 import { LoggerModule } from './common/logger/logger.module';
 @Module({
   imports: [LoggerModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class AppModule {}
