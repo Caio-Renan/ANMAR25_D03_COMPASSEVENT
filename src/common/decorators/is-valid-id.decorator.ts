2d27247d6f42886083130f7c39c6c565fa6a542a
Caio Renan <caiorenan016@gmail.com>
2025-06-01 21:37:45 -0300

    chore(decorators, tests): remove is-valid-id.decorator and its tests no longer needed

--- src/common/decorators/is-valid-id.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { IsValidInt } from './is-valid-int.decorator';
-import type { ValidationOptions } from 'class-validator';
-
-export function IsValidId(validationOptions?: ValidationOptions) {
-  return IsValidInt(validationOptions);
-}
