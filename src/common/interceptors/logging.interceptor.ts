e76cd6f1678f1724b7f3c42adf626fb777339e63
Caio Renan <caiorenan016@gmail.com>
2025-06-01 22:25:25 -0300

    chore(logger): move logging.interceptor to common/interceptors

--- src/common/interceptors/logging.interceptor.ts
+++ src/common/interceptors/logging.interceptor.ts
@@ -1,7 +1,7 @@
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
 import { Observable, tap } from 'rxjs';
 
-import type { LoggerService } from './logger.service';
+import type { LoggerService } from '../logger/logger.service';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
