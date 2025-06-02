c3ca71ec1061d0c1370af3a6548fbcc425942d05
Caio Renan <caiorenan016@gmail.com>
2025-06-02 01:38:13 -0300

    feat(exceptions): integrate LoggerService into AllExceptionsFilter

--- src/common/exceptions/filters/all-exceptions.filter.ts
+++ src/common/exceptions/filters/all-exceptions.filter.ts
@@ -1,9 +1,15 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, HttpStatus } from '@nestjs/common';
 
 import { ExceptionErrorMessages } from '../../constants/error-messages/exception-error-messages';
+import { LoggerService } from '../../logger/logger.service';
+
 @Catch()
 export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(private readonly logger: LoggerService) {}
+
   catch(exception: unknown, host: ArgumentsHost) {
+    this.logger.error({ exception }, 'Unhandled exception caught in AllExceptionsFilter');
+
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
     const request = ctx.getRequest();
@@ -11,10 +17,14 @@ export class AllExceptionsFilter implements ExceptionFilter {
     const status =
       exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
 
+    const responseMessage = exception instanceof HttpException ? exception.getResponse() : null;
+
     const message =
-      exception instanceof HttpException
-        ? exception.getResponse()
-        : ExceptionErrorMessages.FILTER_EXCEPTION.INTERNAL_SERVER_ERROR;
+      typeof responseMessage === 'string'
+        ? responseMessage
+        : responseMessage && typeof responseMessage === 'object' && 'message' in responseMessage
+          ? (responseMessage as any).message
+          : ExceptionErrorMessages.FILTER_EXCEPTION.INTERNAL_SERVER_ERROR;
 
     response.status(status).json({
       statusCode: status,
