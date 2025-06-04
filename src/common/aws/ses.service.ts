2ab183d934cc75aed79d8a15021960ce0e7c43f0
Caio Renan <caiorenan016@gmail.com>
2025-06-04 14:30:54 -0300

    refactor: improve sendEmail method and add SendEmailPayload interface to SesService

--- src/common/aws/ses.service.ts
+++ src/common/aws/ses.service.ts
@@ -5,6 +5,15 @@ import { ConfigService } from '@nestjs/config';
 import { AWS_CLIENTS } from '../constants/aws.constants';
 import { AwsErrorMessages } from '../constants/error-messages/aws-error-messages';
 
+export interface SendEmailPayload {
+  to: string[];
+  subject: string;
+  htmlBody: string;
+  textBody?: string;
+  cc?: string[];
+  bcc?: string[];
+}
+
 @Injectable()
 export class SESService {
   private readonly logger = new Logger(SESService.name);
@@ -18,31 +27,34 @@ export class SESService {
     this.fromEmail = this.configService.getOrThrow<string>('aws.ses.fromEmail');
   }
 
-  async sendEmail(to: string[], subject: string, htmlBody: string, textBody?: string) {
+  async sendEmail({ to, subject, htmlBody, textBody, cc, bcc }: SendEmailPayload): Promise<string> {
+    if (!to?.length) {
+      throw new Error('Destination email(s) required');
+    }
+
     try {
       const command = new SendEmailCommand({
         Source: this.fromEmail,
         Destination: {
           ToAddresses: to,
+          CcAddresses: cc,
+          BccAddresses: bcc,
         },
         Message: {
-          Subject: {
-            Data: subject,
-          },
+          Subject: { Data: subject },
           Body: {
-            Html: {
-              Data: htmlBody,
-            },
-            ...(textBody && {
-              Text: {
-                Data: textBody,
-              },
-            }),
+            Html: { Data: htmlBody },
+            ...(textBody && { Text: { Data: textBody } }),
           },
         },
       });
 
-      await this.sesClient.send(command);
+      const response = await this.sesClient.send(command);
+      this.logger.log(
+        `Email sent to ${to.join(', ')} with subject "${subject}". MessageId: ${response.MessageId}`,
+      );
+
+      return response.MessageId ?? '';
     } catch (error) {
       this.logger.error(AwsErrorMessages.SES.SEND_EMAIL_ERROR(to, subject), error as Error);
       throw error;
