da061d2cc483b6f30f5a7fd41b904720938ff4d4
Caio Renan <caiorenan016@gmail.com>
2025-08-13 16:04:12 -0300

    feat(config): add S3 bucket and folder paths to configuration and include aws.sessionToken

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,7 +6,7 @@ export default () => ({
     region: process.env.AWS_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    s3BucketName: process.env.AWS_S3_BUCKET_NAME,
+    sessionToken: process.env.AWS_SESSION_TOKEN,
     ses: {
       region: process.env.AWS_SES_REGION,
       fromEmail: process.env.AWS_SES_FROM_EMAIL,
@@ -18,6 +18,14 @@ export default () => ({
     usersTable: process.env.AWS_DYNAMODB_USERS_TABLE,
     eventsTable: process.env.AWS_DYNAMODB_EVENTS_TABLE,
     subscriptionsTable: process.env.AWS_DYNAMODB_SUBSCRIPTIONS_TABLE,
+  },
+
+  s3: {
+    bucketName: process.env.AWS_S3_BUCKET_NAME,
+    usersFolderOriginals: process.env.AWS_S3_FOLDER_USERS_ORIGINALS,
+    usersFolderResized: process.env.AWS_S3_FOLDER_USERS_RESIZED,
+    eventsFolderOriginals: process.env.AWS_S3_FOLDER_EVENTS_ORIGINALS,
+    eventsFolderResized: process.env.AWS_S3_FOLDER_EVENTS_RESIZED,
   },
 
   jwt: {
