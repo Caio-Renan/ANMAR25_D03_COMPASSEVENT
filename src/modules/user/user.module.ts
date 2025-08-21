dac40d6fe42d51c149ea1638f007a738de1cf51c
Caio Renan <caiorenan016@gmail.com>
2025-08-21 10:30:25 -0300

    refactor(user): add EmailModule to imports

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -4,8 +4,10 @@ import { Module } from '@nestjs/common';
 import { UserController } from '@user/controllers/user.controller';
 import { UserRepository } from '@user/repositories/user.repository';
 import { UserService } from '@user/services/user.service';
+
+import { EmailModule } from '../mail/email.module';
 @Module({
-  imports: [AwsModule],
+  imports: [AwsModule, EmailModule],
   controllers: [UserController],
   providers: [UserService, DynamoService, UserRepository],
   exports: [UserService],
