a3d798ffb26038fcce8360df6d3e708beb2f23fb
Caio Renan <caiorenan016@gmail.com>
2025-08-21 09:17:58 -0300

    feat(aws): register JwtModule with env secret and 1h expiration

--- src/config/aws/aws.module.ts
+++ src/config/aws/aws.module.ts
@@ -6,13 +6,20 @@ import { S3Provider } from '@config/aws/s3.provider';
 import { SESProvider } from '@config/aws/ses.provider';
 import { Global, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 const awsProviders = [DynamoProvider, S3Provider, SESProvider];
 const awsServices = [DynamoService, S3Service, SESService];
 
 @Global()
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+    ConfigModule,
+  ],
   providers: [...awsProviders, ...awsServices],
   exports: [...awsProviders, ...awsServices],
 })
